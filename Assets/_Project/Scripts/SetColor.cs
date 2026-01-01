2466517da1efdbb60cf236e8b6aa0362275388ac
Andrea Fraboni <151090404+AndreaFraboni@users.noreply.github.com>
2026-01-01 02:19:07 +0100

    Better use of OnMouseOver instead Update to reset default color

--- Assets/_Project/Scripts/SetColor.cs
+++ Assets/_Project/Scripts/SetColor.cs
@@ -1,31 +1,40 @@
-using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class SetColor : MonoBehaviour
 {
     [SerializeField] private ColorManager _cm;
-    private MeshRenderer _mr;
+    [SerializeField] private MeshRenderer _mr;
+    [SerializeField] private Color _defaultColor;
 
     private void Awake()
     {
-        _cm = FindAnyObjectByType<ColorManager>();
+        if (_cm == null) _cm = FindAnyObjectByType<ColorManager>();
+        if (_mr == null) _mr = GetComponentInParent<MeshRenderer>();
     }
 
     private void OnMouseDown()
     {
         if (Input.GetMouseButton(0))
         {
-            MeshRenderer _meshRenderer = this.GetComponentInParent<MeshRenderer>();
-            _meshRenderer.material.color = _cm.currentColor;
+            _mr.material.color = _cm.currentColor;
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            _mr.material.SetColor("_BaseColor", Color.gray);
         }
     }
 
     private void Update()
     {
-        if (Input.GetMouseButton(1))
-        {
-            MeshRenderer _meshRenderer = this.GetComponentInParent<MeshRenderer>();
-            _meshRenderer.material.SetColor("_BaseColor", Color.gray);
-        }
+        // Non resetto qui perchè altrimenti resetta tutti i cubi con lo stesso colore inziale
+        //if (Input.GetMouseButton(1))
+        //{
+        //    _mr = this.GetComponentInParent<MeshRenderer>();
+        //    _mr.material.SetColor("_BaseColor", Color.gray);
+        //}
     }
 }
